75f6535b7381430de940951984b0d4e349bcfcde
poy004id <agungsupoyo@gmail.com>
2020-12-18 11:15:53 +0700

    update sql database, tambahkan semua table

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 14, 2020 at 03:59 PM
+-- Generation Time: Dec 18, 2020 at 05:13 AM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -21,15 +21,38 @@ SET time_zone = "+00:00";
 --
 -- Database: `wartegnusantara`
 --
-CREATE DATABASE IF NOT EXISTS `wartegnusantara` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `wartegnusantara`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bahanmakanan`
+--
+
+CREATE TABLE `bahanmakanan` (
+  `ID` int(10) NOT NULL,
+  `NamaBahanBaku` varchar(50) NOT NULL,
+  `Qty` int(4) NOT NULL,
+  `KategoriBahanMakananID` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detailpemesanan`
+--
+
+CREATE TABLE `detailpemesanan` (
+  `ID` int(10) NOT NULL,
+  `PemesananID` int(10) NOT NULL,
+  `MenuMakananID` int(10) NOT NULL,
+  `Qty` int(4) NOT NULL,
+  `Harga` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `emailconfig`
---
--- Creation: Nov 30, 2020 at 06:54 AM
 --
 
 CREATE TABLE `emailconfig` (
@@ -56,9 +79,40 @@ INSERT INTO `emailconfig` (`id`, `fromemail`, `fromname`, `protocol`, `host`, `u
 -- --------------------------------------------------------
 
 --
--- Table structure for table `logs`
+-- Table structure for table `job`
 --
--- Creation: Nov 30, 2020 at 06:54 AM
+
+CREATE TABLE `job` (
+  `ID` int(10) NOT NULL,
+  `NamaJob` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategoribahanmakanan`
+--
+
+CREATE TABLE `kategoribahanmakanan` (
+  `ID` int(10) NOT NULL,
+  `NamaKategory` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategorimakanan`
+--
+
+CREATE TABLE `kategorimakanan` (
+  `ID` int(10) NOT NULL,
+  `KategoryMakanan` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logs`
 --
 
 CREATE TABLE `logs` (
@@ -90,14 +144,61 @@ INSERT INTO `logs` (`id`, `date`, `time`, `reference`, `name`, `ip`, `location`,
 (20, '2020-12-14', '09:44:39', 1, 'Admin', '::1', NULL, 'Chrome', 'Success', '2020-12-14 09:44:39', '2020-12-14 09:44:39'),
 (21, '2020-12-14', '09:48:57', 1, 'Admin', '::1', NULL, 'Chrome', 'Success', '2020-12-14 09:48:57', '2020-12-14 09:48:57'),
 (22, '2020-12-14', '09:54:54', 1, 'Admin', '::1', NULL, 'Chrome', 'Success', '2020-12-14 09:54:54', '2020-12-14 09:54:54'),
-(23, '2020-12-14', '10:07:27', 1, 'Admin', '::1', NULL, 'Chrome', 'Success', '2020-12-14 10:07:27', '2020-12-14 10:07:27');
+(23, '2020-12-14', '10:07:27', 1, 'Admin', '::1', NULL, 'Chrome', 'Success', '2020-12-14 10:07:27', '2020-12-14 10:07:27'),
+(24, '2020-12-17', '15:26:50', 1, 'Admin', '::1', NULL, 'Chrome', 'Success', '2020-12-17 15:26:50', '2020-12-17 15:26:50'),
+(25, '2020-12-17', '17:12:59', 1, 'Admin', '::1', NULL, 'Chrome', 'Success', '2020-12-17 17:12:59', '2020-12-17 17:12:59'),
+(26, '2020-12-17', '20:41:59', 1, 'Admin', '::1', NULL, 'Chrome', 'Success', '2020-12-17 20:41:59', '2020-12-17 20:41:59'),
+(27, '2020-12-17', '20:42:19', 1, 'Admin', '::1', NULL, 'Chrome', 'Success', '2020-12-17 20:42:19', '2020-12-17 20:42:19'),
+(28, '2020-12-17', '20:46:19', 1, 'Admin', '::1', NULL, 'Chrome', 'Success', '2020-12-17 20:46:19', '2020-12-17 20:46:19'),
+(29, '2020-12-17', '21:14:01', 1, 'Admin', '::1', NULL, 'Chrome', 'Success', '2020-12-17 21:14:01', '2020-12-17 21:14:01');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menumakanan`
+--
+
+CREATE TABLE `menumakanan` (
+  `ID` int(10) NOT NULL,
+  `Nama` varchar(255) NOT NULL,
+  `Qty` int(10) NOT NULL,
+  `Keterangan` varchar(255) DEFAULT NULL,
+  `HargaPerPorsi` int(5) NOT NULL,
+  `KategoriMakananID` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pemesanan`
+--
+
+CREATE TABLE `pemesanan` (
+  `ID` int(10) NOT NULL,
+  `PembeliID` int(10) NOT NULL,
+  `Tanggal` date NOT NULL,
+  `TotalBayar` int(10) NOT NULL,
+  `UserID` int(10) NOT NULL,
+  `usersID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `resep`
+--
+
+CREATE TABLE `resep` (
+  `ID` int(10) NOT NULL,
+  `MenuMakananID` int(10) NOT NULL,
+  `BahanMakananID` int(10) NOT NULL,
+  `Qty` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `settings`
---
--- Creation: Nov 30, 2020 at 06:54 AM
 --
 
 CREATE TABLE `settings` (
@@ -123,16 +224,14 @@ INSERT INTO `settings` (`id`, `language`, `timezone`, `dateformat`, `timeformat`
 --
 -- Table structure for table `users`
 --
--- Creation: Nov 30, 2020 at 06:54 AM
---
 
 CREATE TABLE `users` (
-  `id` int(11) UNSIGNED NOT NULL,
+  `id` int(11) NOT NULL,
   `email` varchar(191) COLLATE utf8_unicode_ci NOT NULL,
   `new_email` varchar(191) COLLATE utf8_unicode_ci DEFAULT NULL,
   `password_hash` varchar(191) COLLATE utf8_unicode_ci NOT NULL,
   `name` varchar(191) COLLATE utf8_unicode_ci NOT NULL,
-  `firstname` varchar(191) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `JobID` int(10) DEFAULT NULL,
   `lastname` varchar(191) COLLATE utf8_unicode_ci DEFAULT NULL,
   `activate_hash` varchar(191) COLLATE utf8_unicode_ci DEFAULT NULL,
   `reset_hash` varchar(191) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -146,12 +245,27 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `email`, `new_email`, `password_hash`, `name`, `firstname`, `lastname`, `activate_hash`, `reset_hash`, `reset_expires`, `active`, `created_at`, `updated_at`) VALUES
-(1, 'admin@example.com', NULL, '$2y$10$/clmNLUUxGnREFHqraU7P.aVfJ5Mk0iEKRgKVz8.ZKOZIUGJGXlya', 'Admin', 'Admin', 'User', 'ZEWv2TUIY5oDqgw9FkjnmAS78zJNKfpL', NULL, NULL, 1, '2020-08-04 16:07:50', '2020-09-11 14:32:51');
+INSERT INTO `users` (`id`, `email`, `new_email`, `password_hash`, `name`, `JobID`, `lastname`, `activate_hash`, `reset_hash`, `reset_expires`, `active`, `created_at`, `updated_at`) VALUES
+(1, 'admin@example.com', NULL, '$2y$10$/clmNLUUxGnREFHqraU7P.aVfJ5Mk0iEKRgKVz8.ZKOZIUGJGXlya', 'Admin', 0, 'User', 'ZEWv2TUIY5oDqgw9FkjnmAS78zJNKfpL', NULL, NULL, 1, '2020-08-04 16:07:50', '2020-09-11 14:32:51');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `bahanmakanan`
+--
+ALTER TABLE `bahanmakanan`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `FKBahanMakan152387` (`KategoriBahanMakananID`);
+
+--
+-- Indexes for table `detailpemesanan`
+--
+ALTER TABLE `detailpemesanan`
+  ADD PRIMARY KEY (`ID`,`PemesananID`,`MenuMakananID`),
+  ADD KEY `FKDetailPeme798786` (`PemesananID`),
+  ADD KEY `FKDetailPeme568681` (`MenuMakananID`);
 
 --
 -- Indexes for table `emailconfig`
@@ -160,10 +274,51 @@ ALTER TABLE `emailconfig`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `job`
+--
+ALTER TABLE `job`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `kategoribahanmakanan`
+--
+ALTER TABLE `kategoribahanmakanan`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `kategorimakanan`
+--
+ALTER TABLE `kategorimakanan`
+  ADD PRIMARY KEY (`ID`);
+
+--
 -- Indexes for table `logs`
 --
 ALTER TABLE `logs`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `menumakanan`
+--
+ALTER TABLE `menumakanan`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `FKMenuMakana417765` (`KategoriMakananID`);
+
+--
+-- Indexes for table `pemesanan`
+--
+ALTER TABLE `pemesanan`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `FKPemesanan601987` (`UserID`),
+  ADD KEY `usersID` (`usersID`);
+
+--
+-- Indexes for table `resep`
+--
+ALTER TABLE `resep`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `FKResep482739` (`MenuMakananID`),
+  ADD KEY `FKResep338545` (`BahanMakananID`);
 
 --
 -- Indexes for table `settings`
@@ -175,11 +330,18 @@ ALTER TABLE `settings`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `JobID` (`JobID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `bahanmakanan`
+--
+ALTER TABLE `bahanmakanan`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `emailconfig`
@@ -188,10 +350,46 @@ ALTER TABLE `emailconfig`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `job`
+--
+ALTER TABLE `job`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `kategoribahanmakanan`
+--
+ALTER TABLE `kategoribahanmakanan`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `kategorimakanan`
+--
+ALTER TABLE `kategorimakanan`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `logs`
 --
 ALTER TABLE `logs`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- AUTO_INCREMENT for table `menumakanan`
+--
+ALTER TABLE `menumakanan`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pemesanan`
+--
+ALTER TABLE `pemesanan`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `resep`
+--
+ALTER TABLE `resep`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `settings`
@@ -203,7 +401,49 @@ ALTER TABLE `settings`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bahanmakanan`
+--
+ALTER TABLE `bahanmakanan`
+  ADD CONSTRAINT `FKBahanMakan152387` FOREIGN KEY (`KategoriBahanMakananID`) REFERENCES `kategoribahanmakanan` (`ID`);
+
+--
+-- Constraints for table `detailpemesanan`
+--
+ALTER TABLE `detailpemesanan`
+  ADD CONSTRAINT `FKDetailPeme568681` FOREIGN KEY (`MenuMakananID`) REFERENCES `menumakanan` (`ID`),
+  ADD CONSTRAINT `FKDetailPeme798786` FOREIGN KEY (`PemesananID`) REFERENCES `pemesanan` (`ID`);
+
+--
+-- Constraints for table `job`
+--
+ALTER TABLE `job`
+  ADD CONSTRAINT `job_ibfk_1` FOREIGN KEY (`ID`) REFERENCES `users` (`JobID`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Constraints for table `menumakanan`
+--
+ALTER TABLE `menumakanan`
+  ADD CONSTRAINT `FKMenuMakana417765` FOREIGN KEY (`KategoriMakananID`) REFERENCES `kategorimakanan` (`ID`);
+
+--
+-- Constraints for table `resep`
+--
+ALTER TABLE `resep`
+  ADD CONSTRAINT `FKResep338545` FOREIGN KEY (`BahanMakananID`) REFERENCES `bahanmakanan` (`ID`),
+  ADD CONSTRAINT `FKResep482739` FOREIGN KEY (`MenuMakananID`) REFERENCES `menumakanan` (`ID`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`id`) REFERENCES `pemesanan` (`usersID`) ON DELETE NO ACTION ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
