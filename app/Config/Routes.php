f7c228b3ce99b60d062548054119c278fe134ecb
poyoagung <76144716+poyoagung@users.noreply.github.com>
2021-01-01 22:57:33 +0700

    Authentication Users

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,6 +31,11 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Dashboard::index');
+$routes->get('/kategori_bahan', 'Kategori_Bahan::index',['filter'=>'auth']);
+$routes->get('/bahan', 'Bahan::index',['filter'=>'auth']);
+$routes->get('/kategori_bahan2', 'Kategori_Bahan2::index',['filter'=>'auth']);
+$routes->get('/kategori_menu', 'Kategori_Menu::index',['filter'=>'auth']);
+$routes->get('/menu', 'Menu::index',['filter'=>'auth']);
 
 /**
  * --------------------------------------------------------------------
