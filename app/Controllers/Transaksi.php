bed971426937c0b4279c1e449dc0a0b5daee9e22
poyoagung <76144716+poyoagung@users.noreply.github.com>
2021-01-03 17:10:57 +0700

    Update Transaksi.php

--- app/Controllers/Transaksi.php
+++ app/Controllers/Transaksi.php
@@ -8,148 +8,99 @@ use App\Models\Menu_model;
 
 class Transaksi extends BaseController
 {
-
+	protected $db, $builder;
 	public function __construct()
     {
 		$session = \Config\Services::session();
-		//
-		// if(session()->get('level') != "Admin" && session()->get('status') != "Active"){
-		// 	session()->setFlashdata('errors', ['' => 'Silahkan login terlebih dahulu untuk mengakses data.']);
-		// 	return redirect()->to('/auth/login');
-		// 	}
-		//
-		// 	echo"123";
-		// 	exit;
-		}
+		helper('form');
+		$this->db 	 = \Config\Database::connect();
+		$this->builder = $this->db->table('transaksi');
+	}
 
-	public function index()
-	{
-		 $data['userdata'] = session('username');
-		 $model= new Transaksi_model();
-		 $data['transaksi'] = $model->select('transaksi.id as id, nama_menu,detail_transaksi.jumlah as jumlah,menu.harga as harga,total_harga')
-
-                            ->join('detail_transaksi', 'transaksi.id=detail_transaksi.id_transaksi')
-                            ->join('menu', 'detail_transaksi.id_menu=menu.id')
-		 												->findAll();
-
-                            // echo "<pre>";
-                            // foreach ($data['transaksi'] as $key => $row) {
-                            //   print_r($row);
-                            //   echo "<br/>";
-                            // }
-                            //
-                            // exit;
-
-
+	public function index(){
+		$data['userdata'] = session('username');
+		$this->builder->select('transaksi.id as transaksi_id, tanggal,total_harga, id_user');
+		$query=$this->builder->get(); 
+		$data['item'] = $query->getResult();
 		echo view('_partials/header',$data);
 		echo view('_partials/sidebar',$data);
-    echo view('transaksi/index',$data);
+    	echo view('transaksi/index',$data);
 		echo view('_partials/footer');
     }
 
-		public function create()
-		{
-			$transaksimodel= new Detail_Transaksi_model();
-      $menumodel= new Menu_model();
-			$data['userdata'] = session('username');
-			$data['menu'] = $menumodel->findAll();
-      $data['tansaksi'] = $transaksimodel->findAll();
-      // echo view('_partials/header',$data);
-			// echo view('_partials/sidebar',$data);
-			echo view('transaksi/create',$data);
-			echo view('_partials/footer');
+	public function create()
+	{
+		$transaksidetailmodel= new Detail_Transaksi_model();
+		$transaksimodel= new Transaksi_model();
+  		$menumodel= new Menu_model();
+		$data['userdata'] = session('username');
+		$data['cart'] = \Config\Services::cart();
+		$data['menu'] = $menumodel->findAll();
+		$data['transaksi'] = $transaksidetailmodel->findAll();
+		$data['id']=$transaksimodel->get_nofak();
+		
+  		echo view('_partials/header',$data);
+		echo view('_partials/sidebar',$data);
+		echo view('transaksi/create',$data);
+		echo view('_partials/footer');
 
-		}
-
-		// public function store()
-		// {
-		// 	$model= new Menu_model();
-		// 	$validation =  \Config\Services::validation();
-    //
-		// 	$data = [
-		// 				'id' => $this->request->getPost('id'),
-		// 				'nama_menu' => $this->request->getPost('nama_menu'),
-		// 				'jumlah' => $this->request->getPost('jumlah'),
-		// 				'harga' => $this->request->getPost('harga'),
-		// 				'keterangan' => $this->request->getPost('keterangan'),
-		// 				'id_kategori_menu' => $this->request->getPost('id_kategori_menu'),
-		// 		];
-		// 	//cek validasi
-		// 	if($validation->run($data, 'menu') == FALSE){
-		// 			session()->setFlashdata('inputs', $this->request->getPost());
-		// 			session()->setFlashdata('errors', $validation->getErrors());
-		// 			return redirect()->to(base_url('menu/create'));
-		// 		}
-		// 		//preses insert ke db
-		// 	$model->insert($data);
-		// 	session()->setFlashdata('success', 'Menu berhasil ditambahakn');
-		// 	return redirect()->to('/menu');
-		// }
-    //
-		// public function edit($id=FALSE)
-		// 		{
-		// 			$model = new Menu_model();
-		// 			$kategori_model = new Kategori_Menu_model();
-    //
-		// 			$data = array(
-		// 					'menu' => $model->find($id),
-		// 					'kategoriData' => $kategori_model->findAll(),
-		// 			);
-		// 			$data['userdata'] = session('username');
-    //
-		// 				echo view('_partials/header', $data);
-		// 				echo view('_partials/sidebar');
-		// 				echo view('menu/edit', $data);
-		// 			 echo view('_partials/footer');
-		// 		}
-    //
-		// public function update()
-		// {
-		// 	$id = $this->request->getPost('id');
-		// 	$model= new Menu_model();
-		// 	$validation =  \Config\Services::validation();
-    //
-		// 	$data = [
-		// 				'id' => $this->request->getPost('id'),
-		// 				'nama_menu' => $this->request->getPost('nama_menu'),
-		// 				'jumlah' => $this->request->getPost('jumlah'),
-		// 				'harga' => $this->request->getPost('harga'),
-		// 				'keterangan' => $this->request->getPost('keterangan'),
-		// 				'id_kategori_menu' => $this->request->getPost('id_kategori_menu'),
-		// 		];
-    //
-		// 		// echo "<pre>";
-		// 		// foreach ($data as $key => $row) {
-		// 		//   print_r($row);
-		// 		//   echo "<br/>";
-		// 		// }
-		// 		//
-		// 		// exit;
-    //
-    //
-		// 	//cek validasi
-		// 	if($validation->run($data, 'menu') == FALSE){
-		// 			session()->setFlashdata('inputs', $this->request->getPost());
-		// 			session()->setFlashdata('errors', $validation->getErrors());
-		// 			return redirect()->to(base_url('menu/edit'));
-		// 			// return redirect(base_url()."menu/edit/".$id);
-    //
-		// 		}
-    //
-		// 	//proses update ke db
-		// 	$model->update($id,$data);
-		// 	session()->setFlashdata('success', 'Menu Makanan berhasil diupdate');
-		// 	return redirect()->to('/menu');
-		// }
-    //
-		// public function delete($id)
-		// {
-    //
-		// 	$model= new Menu_model();
-		// 	$model 	->where('id', $id)
-		// 				  ->delete();
-		// 	session()->setFlashdata('success', 'Menu Makanan berhasil di delete');
-		// 	return redirect()->to('/menu');
-		// }
+	}
+	public function cek(){
+		$cart = \Config\Services::cart();
+		$response = $cart->contents();
+		$data = json_encode($response);
+		echo'<pre>';
+		print_r($data);
+		echo'<pre>';
+	}
+	public function add_cart(){
+		$menumodel= new Menu_model();
+		$cart = \Config\Services::cart();
+		$id = $this->request->getPost('id_menu');
+		$produk=$menumodel->find($id);
+		$data = array(
+               'id'       => $produk['id'],
+               'name'     => $produk['nama_menu'],
+               'price'    => $produk['harga'],
+               'qty'      => $this->request->getPost('jumlah'),
+            );
+		$cart->insert($data);
+		return redirect()->to('create');
+	}
+	public function remove($rowid){
+		$cart = \Config\Services::cart();
+    	$cart->remove($rowid);
+    	return redirect()->to(base_url('transaksi/create'));
+  	}
+  	public function simpan(){
+  		$cart = \Config\Services::cart();
+  		$transaksimodel= new Transaksi_model();
+  		$tanggal= $this->request->getPost('tanggal');
+  		$kasir= $this->request->getPost('kasir');
+  		$total= $this->request->getPost('total');
+  		$nofak= $this->request->getPost('nofak');
+		// $jml_uang=str_replace(",", "", $this->request->getPost('jml_uang'));
+		// $kembalian=$jml_uang-$total;
+		
+		
+		$order_proses=$transaksimodel->simpan_penjualan($nofak,$total,$tanggal, $kasir);
+		$cart->destroy();
+			
+		return redirect()->to(base_url('transaksi'));	
+		
+  	}
+  	public function detail($transaksi_id){
+  		$data['userdata'] = session('username');
+		$this->builder->select('transaksi.id as transaksi_id, tanggal, transaksi.id_user as kasir, total_harga, id_menu, nama_menu, detail_transaksi.harga as price, detail_transaksi.jumlah as qty');
+		$this->builder->join('detail_transaksi','detail_transaksi.id_transaksi=transaksi.id');
+		$this->builder->join('menu','menu.id=detail_transaksi.id_menu');
+		$this->builder->where('transaksi.id',$transaksi_id);
+		$query=$this->builder->get(); 
+		$data['item'] = $query->getResult();
+		echo view('_partials/header',$data);
+		echo view('_partials/sidebar',$data);
+    	echo view('transaksi/detail',$data);
+		echo view('_partials/footer');
+  	}
 
 }
