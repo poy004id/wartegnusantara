288b0ac90d1961f29ff420dcb8908bbd51b59c69
Husnulk19 <chuznulcotim@gmail.com>
2021-01-04 20:14:55 +0700

    Update 4jan811.sql

--- 4jan811.sql
+++ 4jan811.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 04, 2021 at 02:11 PM
+-- Generation Time: Jan 04, 2021 at 02:14 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.2.15
 
@@ -25,6 +25,144 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `bahan`
+--
+
+CREATE TABLE `bahan` (
+  `id` int(10) NOT NULL,
+  `nama_bahan` varchar(50) NOT NULL,
+  `jumlah` int(4) NOT NULL,
+  `satuan` varchar(10) NOT NULL,
+  `id_kategori_bahan` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `bahan`
+--
+
+INSERT INTO `bahan` (`id`, `nama_bahan`, `jumlah`, `satuan`, `id_kategori_bahan`) VALUES
+(1, 'Berass', 4556, 'kg', 1),
+(2, 'Gula', 40, 'kg', 2),
+(3, 'Garam', 500, 'gram', 2),
+(5, 'Kangkung', 18, 'ikat', 5),
+(7, 'Bawang merah', 20, 'kg', 2),
+(8, 'Teh', 100, 'pack', 7);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_transaksi`
+--
+
+CREATE TABLE `detail_transaksi` (
+  `id` int(10) NOT NULL,
+  `id_transaksi` int(11) NOT NULL,
+  `id_menu` int(10) NOT NULL,
+  `jumlah` int(4) NOT NULL,
+  `harga` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `detail_transaksi`
+--
+
+INSERT INTO `detail_transaksi` (`id`, `id_transaksi`, `id_menu`, `jumlah`, `harga`) VALUES
+(1, 301210001, 1, 30, 3000),
+(2, 301210002, 1, 20, 3000),
+(3, 301210003, 2, 10, 3000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_transaksi_temp`
+--
+
+CREATE TABLE `detail_transaksi_temp` (
+  `id_menu` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori_bahan`
+--
+
+CREATE TABLE `kategori_bahan` (
+  `id` int(10) NOT NULL,
+  `nama_kategori` varchar(100) NOT NULL,
+  `status` enum('active','inactive') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `kategori_bahan`
+--
+
+INSERT INTO `kategori_bahan` (`id`, `nama_kategori`, `status`) VALUES
+(1, 'Makanan Utama', 'inactive'),
+(2, 'Bumbu', 'active'),
+(5, 'Sayuran', 'active'),
+(6, 'Buah', 'active'),
+(7, 'Minuman', 'active'),
+(14, 'Lain lain', 'active'),
+(15, 'Sambal ', 'inactive'),
+(16, 'Sambal', 'active');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori_menu`
+--
+
+CREATE TABLE `kategori_menu` (
+  `id` int(10) NOT NULL,
+  `nama_kategori` varchar(100) NOT NULL,
+  `status` enum('active','inactive') NOT NULL DEFAULT 'active'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `kategori_menu`
+--
+
+INSERT INTO `kategori_menu` (`id`, `nama_kategori`, `status`) VALUES
+(1, 'Makanan Utama', 'active'),
+(3, 'Lauk Pauk', 'active'),
+(4, 'Sayur Mayur', 'active'),
+(5, 'Sambal', 'active'),
+(6, 'Gorengan', 'active'),
+(7, 'Minuman', 'active');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu`
+--
+
+CREATE TABLE `menu` (
+  `id` int(10) NOT NULL,
+  `nama_menu` varchar(255) NOT NULL,
+  `jumlah` int(10) NOT NULL,
+  `keterangan` varchar(255) DEFAULT NULL,
+  `harga` int(5) NOT NULL,
+  `id_kategori_menu` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `menu`
+--
+
+INSERT INTO `menu` (`id`, `nama_menu`, `jumlah`, `keterangan`, `harga`, `id_kategori_menu`) VALUES
+(1, 'Nasi putih', 20, 'cuma nasi biasa aja bukan nasi kaya', 3000, 1),
+(2, 'Nasi uduk', 20, 'rasanya gurih', 3000, 1),
+(3, 'Ayam goreng sambal ijo', 28, 'pedas level dewa', 8000, 3),
+(5, 'Orek Tempe', 0, 'murrahahah', 500, 3),
+(6, 'Semur Jengkol', 87, 'dijamin nagih', 5000, 4),
+(7, 'Nasi Kucing', 100, '', 2000, 1);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `resep`
 --
 
@@ -35,9 +173,89 @@ CREATE TABLE `resep` (
   `jumlah` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id` varchar(20) NOT NULL,
+  `tanggal` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `total_harga` int(10) NOT NULL,
+  `id_user` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id`, `tanggal`, `total_harga`, `id_user`) VALUES
+('0301210001', '2021-01-02 17:00:00', 90000, 'admin'),
+('0301210002', '2021-01-02 17:00:00', 30000, 'admin'),
+('0301210003', '2021-01-02 17:00:00', 30000, 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(10) NOT NULL,
+  `username` varchar(15) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `No_hp` varchar(255) NOT NULL,
+  `status` enum('active','inactive') NOT NULL DEFAULT 'active',
+  `level` enum('admin','kasir','koki') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `password`, `nama`, `No_hp`, `status`, `level`) VALUES
+(2, 'admin', '$2y$10$O4cuuAOVOAtOq2rxMfErrua7FFSHlKOJDLYqhDFntaEtVTm/5LB9W', 'admin', '1234', 'active', 'admin');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `bahan`
+--
+ALTER TABLE `bahan`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nama_bahan` (`nama_bahan`),
+  ADD KEY `FKbahan524060` (`id_kategori_bahan`);
+
+--
+-- Indexes for table `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- Indexes for table `kategori_bahan`
+--
+ALTER TABLE `kategori_bahan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kategori_menu`
+--
+ALTER TABLE `kategori_menu`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nama_kategori` (`nama_kategori`);
+
+--
+-- Indexes for table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nama_menu` (`nama_menu`),
+  ADD KEY `FKmenu181546` (`id_kategori_menu`);
 
 --
 -- Indexes for table `resep`
@@ -48,8 +266,51 @@ ALTER TABLE `resep`
   ADD KEY `FKresep471503` (`id_bahan`);
 
 --
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `bahan`
+--
+ALTER TABLE `bahan`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `kategori_bahan`
+--
+ALTER TABLE `kategori_bahan`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `kategori_menu`
+--
+ALTER TABLE `kategori_menu`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `resep`
@@ -58,8 +319,26 @@ ALTER TABLE `resep`
   MODIFY `id_resep` int(10) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `bahan`
+--
+ALTER TABLE `bahan`
+  ADD CONSTRAINT `FKbahan524060` FOREIGN KEY (`id_kategori_bahan`) REFERENCES `kategori_bahan` (`id`);
+
+--
+-- Constraints for table `menu`
+--
+ALTER TABLE `menu`
+  ADD CONSTRAINT `FKmenu181546` FOREIGN KEY (`id_kategori_menu`) REFERENCES `kategori_menu` (`id`);
 
 --
 -- Constraints for table `resep`
